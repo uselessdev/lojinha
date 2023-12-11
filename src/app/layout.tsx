04054c11b39ca12d7b821d31ac88c9b7511f69b0
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-12-11 16:13:16 -0300

    feat(app): add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,44 @@
 import * as React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ptBR } from "@clerk/localizations";
+import { Metadata, Viewport } from "next";
 import { ThemeProvider } from "~/components/theme/provider";
 import "~/app/globals.css";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  colorScheme: "light dark",
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#000000" },
+    { media: "(prefers-color-scheme: light)", color: "#fefefe" },
+  ],
+};
+
+export const metadata: Metadata = {
+  title: {
+    default: "lojinha",
+    template: "%s | Lojinha",
+  },
+  description: "Uma plataforma de e-commerce headless para desenvolvedores front-end.",
+  icons: [
+    { rel: "apple-touch-icon", url: "./apple-touch-icon.png" },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+  ],
+  manifest: "/site.webmanifest",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
